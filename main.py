c3e0410cfb04e0f89107efcf50f2700babbe73cc
Gabriel Blanco <gabriel.blancogarcia98@gmail.com>
2022-10-31 19:32:50 +0100

    Add some basic code to typecheck

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.utils import sum_2_numbers
+
+a = 1
+b = 1
+
+result = sum_2_numbers(a, b)
+
+print(result)
---
+++ src/utils.py
@@ -0,0 +1,3 @@
+def sum_2_numbers(a, b):
+    """This function sums 2 numbers"""
+    return a + b
