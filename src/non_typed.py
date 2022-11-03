6af7c7c0fbad2278acbe3b20c117fadcdf0c95bb
Gabriel Blanco <gabriel.blancogarcia98@gmail.com>
2022-11-03 18:43:29 +0100

    Fix bug in path

--- src/non_typed.py
+++ src/non_typed.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 def get_users_data():
-    return json.loads(Path("examples", "data.json").read_text())
+    return json.loads(Path("data", "data.json").read_text())
 
 
 def get_max_age(users):
